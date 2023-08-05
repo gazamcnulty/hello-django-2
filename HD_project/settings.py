9934a2e227eddb5254d70fc6891798bba0df2735
gazamcnulty <gazamcnulty@gmail.com>
2023-08-05 15:29:06 +0000

    issues with csrf

--- HD_project/settings.py
+++ HD_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kfuh1c*f=9l-2=f+^zjd*pu$3dl4whf5jgd!yg0d6^fb!56daq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-gazamcnulty-hellodjango-jnksz2e21gm.ws-eu102.gitpod.io', 'http://8000-gazamcnulty-hellodjango-jnksz2e21gm.ws-eu102.gitpod.io/']
+ALLOWED_HOSTS = ['8000-gazamcnulty-hellodjango-jnksz2e21gm.ws-eu102.gitpod.io', '8000-gazamcnulty-hellodjango-d15ub1rfuoo.ws-eu102.gitpod.io' , '8000-gazamcnulty-hellodjango-d15ub1rfuoo.ws-eu102.gitpod.io'] 
 
 
 # Application definition
